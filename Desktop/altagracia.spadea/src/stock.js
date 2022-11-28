b7a6e4203cdec41ad0ab438e80d837df6facb6be
gabrielspadea <gabrielspadea@hotmail.com>
2022-11-28 02:16:37 -0300

    tercera entrega javascript

---
+++ Desktop/altagracia.spadea/src/stock.js
@@ -0,0 +1,70 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Manta Osito",
+        tipo: "Manta",
+        desc: "La mas suave y calentita",
+        precio: 3000,
+        talle: "90cm",
+        img: './public/images/manta.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Manta Algodon",
+        tipo: "Manta",
+        desc: "Especial para un paseo",
+        precio: 2500,
+        talle: "70cm",
+        img: './public/images/manta1.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Manta Diseño",
+        tipo: "Manta",
+        desc: "Personalizá tu manta",
+        precio: 3500,
+        talle: "75cm",
+        img: './public/images/manta2.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Carpita Tipi",
+        tipo: "carpita",
+        desc: "El mejor regalo",
+        precio: 8000,
+        talle: "1mt x 1,60mt",
+        img: './public/images/carpita.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Hamaca Nido",
+        tipo: "Hamaca",
+        desc: "Especial para el dormitorio",
+        precio: 9000,
+        talle: "1,2mt",
+        img: './public/images/hamaca.jpg',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Nidito",
+        tipo: "nidito",
+        desc: "Super seguro para tu bebé",
+        precio: 6000,
+        talle: "75cm",
+        img: './public/images/nidito1.jpg',
+        cantidad:1
+    }
+]
+
+export { productos };
